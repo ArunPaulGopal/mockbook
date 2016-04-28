bfd6212f2757d0b387c4817d8f2fb572346e4940
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-04-28 12:48:35 -0700

    Issue #6: Images of current friends now appended to the timeline under profile picture.

--- profile/profile.controller.js
+++ profile/profile.controller.js
@@ -7,10 +7,16 @@ app.$inject = ['$http'];
 function profile($http) {
   var vm = this;
 
+  function start() {
+    getFriends();
+  }
+
+  start();
+
   function getFriends() {
-    var friends = $http.get('http://localhost:8080/search');
+    var friends = $http.get('http://localhost:8080/friends');
     friends.then(function(results) {
-      
+      vm.friends = results.data;
     })
   }
 }
