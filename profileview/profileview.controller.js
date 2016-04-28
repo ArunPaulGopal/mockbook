f9c4e042a4c8b6e756f1b7587668ddc2e2bdee86
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-04-28 14:43:14 -0700

    Issue #9: Profile data now retrieved and added to page upon entry.

--- profileview/profileview.controller.js
+++ profileview/profileview.controller.js
@@ -6,15 +6,25 @@ app.$inject = ['$http','$location'];
 
 function profileview($http,$location) {
   var vm = this;
+  var searchObject = '';
 
   function start() {
-    viewProfile();
+    getID();
+    getProfile();
   }
 
   start();
 
-  function viewProfile() {
-    var searchObject = $location.search();
-    console.log(searchObject);
+  function getID() {
+    searchObject = $location.search().id;
+  }
+
+  function getProfile() {
+    var toSearch = {};
+    toSearch.content = searchObject;
+    var profile = $http.post('http://localhost:8080/profile', toSearch);
+    profile.then(function(user){
+      vm.profile =user.data
+    })
   }
 }
