1a15c231e8a4c0c303c9883547e5b8821d57db4e
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-04-29 13:28:59 -0700

    Issue #13: Key functionality complete, video data is displayed as a grid of pictures upon entry of trending videos section. Bootstrap additional styling pending.

--- searchvideos/searchvideos.controller.js
+++ searchvideos/searchvideos.controller.js
@@ -7,7 +7,9 @@ app.$inject = ['$http'];
 function searchvideos($http) {
   var vm = this;
 
-  vm.search = function() {
+  getVideos();
+
+  function getVideos() {
     var searchvideos = $http.get('http://localhost:8080/videos');
     searchvideos.then(function(videos) {
       vm.results = videos.data;
