19b0b29c24fcf5d2bbb967a45f502c16b4c59dec
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-05-12 20:21:30 -0700

    Issue #18: Modifying Flickr request so data is returned as pure JSON instead of callback.

--- searchphotos/searchphotos.controller.js
+++ searchphotos/searchphotos.controller.js
@@ -10,11 +10,9 @@ function searchphotos($http) {
   vm.search = function(value) {
     console.log("Front-end is running");
       api_key: "e3db6f141cc91fc8b844e93484933fd4"
-    var search = $http.get('https://api.flickr.com/services/rest/?&method=flickr.photos.search&text=flower&api_key=e3db6f141cc91fc8b844e93484933fd4&format=json&extras=url_m');
+    var search = $http.get('https://api.flickr.com/services/rest/?&method=flickr.photos.search&text=flower&api_key=e3db6f141cc91fc8b844e93484933fd4&format=json&extras=url_m&nojsoncallback=1');
     search.then(function(res){
-      var test = ''
       vm.results= res.data;
-      console.log(res.data);
     })
   }
 }
