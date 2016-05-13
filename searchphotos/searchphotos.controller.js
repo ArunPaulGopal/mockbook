e7fd07eb21bbad8b2c562dbd130be18d69fe0908
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-05-12 20:08:18 -0700

    Issue #18: Removing code related to Flickr.

--- searchphotos/searchphotos.controller.js
+++ searchphotos/searchphotos.controller.js
@@ -18,13 +18,3 @@ function searchphotos($http) {
     })
   }
 }
-
-
-jsonFlickrApi(
-  {"photos":
-    {"page":1,
-    "pages":5066,
-    "perpage":100,
-    "total":"506551",
-    "photo":[{"id":"26252854444","owner":"74311914@N04","secret":"5125e92508","server":"7187","farm":8,"title":"DSC_6627","ispublic":1,"isfriend":0,"isfamily":0,"url_m":"https:\/\/farm8.staticflickr.com\/7187\/26252854444_5125e92508.jpg","height_m":"334","width_m":"500"},{"id":"26585288000","owner":"128117771@N02","secret":"62cc8cbf51","server":"7422","farm":8,"title":"","ispublic":1,"isfriend":0,"isfamily":0,"url_m":"https:\/\/farm8.staticflickr.com\/7422\/26585288000_62cc8cbf51.jpg","height_m":"334","width_m":"500"}
-  ])
